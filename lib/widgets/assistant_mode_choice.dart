603402826def3f6fc30d341a3c0d08d2f718d6c3
Malik Talha <maliktalha727@outlook.com>
2023-09-24 17:32:22 +0500

    use toggle button instead of segmented buttons

--- lib/widgets/assistant_mode_choice.dart
+++ lib/widgets/assistant_mode_choice.dart
@@ -1,6 +1,75 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import '../models/app_state.dart';
+
+// enum AssistantMode { wakeWord, manual }
+
+// class AssistantModeChoice extends StatefulWidget {
+//   final Function(AssistantMode) onModeChanged;
+
+//   const AssistantModeChoice({Key? key, required this.onModeChanged})
+//       : super(key: key);
+
+//   @override
+//   AssistantModeChoiceState createState() => AssistantModeChoiceState();
+// }
+
+// class AssistantModeChoiceState extends State<AssistantModeChoice> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final appState = context.watch<AppState>(); // Watch the app state
+
+//     return SegmentedButton<AssistantMode>(
+//       segments: const <ButtonSegment<AssistantMode>>[
+//         ButtonSegment<AssistantMode>(
+//             value: AssistantMode.wakeWord,
+//             label: Text('Wake Word'),
+//             icon: Icon(Icons.graphic_eq)),
+//         ButtonSegment<AssistantMode>(
+//             value: AssistantMode.manual,
+//             label: Text('Manual'),
+//             icon: Icon(Icons.graphic_eq)),
+//       ],
+//       selected: <AssistantMode>{
+//         appState.isWakeWordMode ? AssistantMode.wakeWord : AssistantMode.manual
+//       }, // Use app state
+//       onSelectionChanged: (Set<AssistantMode> newSelection) {
+//         final newMode = newSelection.first;
+//         setState(() {
+//           // Update the app state when the mode changes
+//           appState.isWakeWordMode = newMode == AssistantMode.wakeWord;
+//         });
+//         // Call the callback function to notify the mode change
+//         widget.onModeChanged(newMode);
+//       },
+//       style: ButtonStyle(
+//           side: MaterialStateProperty.all<BorderSide>(
+//             BorderSide(
+//               width: 0, // Remove border width
+//               color: Colors.transparent, // Make border transparent
+//             ),
+//           ),
+//           backgroundColor: MaterialStateProperty.resolveWith<Color>(
+//             (states) {
+//               if (states.contains(MaterialState.selected)) {
+//                 return Colors.green; // Color when pressed
+//               }
+//               // Add more conditions for other states as needed
+//               return Colors.white; // Default color
+//             },
+//           ),
+//           foregroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+//             if (states.contains(MaterialState.selected)) {
+//               return Colors.white; // Color when pressed
+//             }
+//             // Add more conditions for other states as needed
+//             return Colors.green;
+//           })),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../models/app_state.dart';
 
 enum AssistantMode { wakeWord, manual }
 
@@ -15,56 +84,117 @@ class AssistantModeChoice extends StatefulWidget {
 }
 
 class AssistantModeChoiceState extends State<AssistantModeChoice> {
+  late AssistantMode _selectedMode;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedMode = AssistantMode.manual; // Initialize the selection
+  }
+
   @override
   Widget build(BuildContext context) {
-    final appState = context.watch<AppState>(); // Watch the app state
-
-    return SegmentedButton<AssistantMode>(
-      segments: const <ButtonSegment<AssistantMode>>[
-        ButtonSegment<AssistantMode>(
-            value: AssistantMode.wakeWord,
-            label: Text('Wake Word'),
-            icon: Icon(Icons.graphic_eq)),
-        ButtonSegment<AssistantMode>(
-            value: AssistantMode.manual,
-            label: Text('Manual'),
-            icon: Icon(Icons.graphic_eq)),
-      ],
-      selected: <AssistantMode>{
-        appState.isWakeWordMode ? AssistantMode.wakeWord : AssistantMode.manual
-      }, // Use app state
-      onSelectionChanged: (Set<AssistantMode> newSelection) {
-        final newMode = newSelection.first;
-        setState(() {
-          // Update the app state when the mode changes
-          appState.isWakeWordMode = newMode == AssistantMode.wakeWord;
-        });
-        // Call the callback function to notify the mode change
-        widget.onModeChanged(newMode);
-      },
-      style: ButtonStyle(
-          side: MaterialStateProperty.all<BorderSide>(
-            BorderSide(
-              width: 0, // Remove border width
-              color: Colors.transparent, // Make border transparent
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        InkWell(
+          onTap: () => _onModeChanged(AssistantMode.wakeWord),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            bottomLeft: Radius.circular(20.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 17.5, vertical: 5.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                bottomLeft: Radius.circular(20.0),
+              ),
+              color: _selectedMode == AssistantMode.wakeWord
+                  ? Colors.green
+                  : Colors.white,
+              border: Border.all(
+                color: Colors.transparent,
+              ),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  _selectedMode == AssistantMode.wakeWord
+                      ? Icons.check
+                      : Icons.graphic_eq,
+                  color: _selectedMode == AssistantMode.wakeWord
+                      ? Colors.white
+                      : Colors.green,
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'Wake Word',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: _selectedMode == AssistantMode.wakeWord
+                        ? Colors.white
+                        : Colors.green,
+                  ),
+                ),
+              ],
             ),
           ),
-          backgroundColor: MaterialStateProperty.resolveWith<Color>(
-            (states) {
-              if (states.contains(MaterialState.selected)) {
-                return Colors.green; // Color when pressed
-              }
-              // Add more conditions for other states as needed
-              return Colors.white; // Default color
-            },
+        ),
+        InkWell(
+          onTap: () => _onModeChanged(AssistantMode.manual),
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            bottomRight: Radius.circular(20.0),
           ),
-          foregroundColor: MaterialStateProperty.resolveWith<Color>((states) {
-            if (states.contains(MaterialState.selected)) {
-              return Colors.white; // Color when pressed
-            }
-            // Add more conditions for other states as needed
-            return Colors.green;
-          })),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 17.5, vertical: 5.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20.0),
+                bottomRight: Radius.circular(20.0),
+              ),
+              color: _selectedMode == AssistantMode.manual
+                  ? Colors.green
+                  : Colors.white,
+              border: Border.all(
+                color: Colors.transparent,
+              ),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  _selectedMode == AssistantMode.manual
+                      ? Icons.check
+                      : Icons.graphic_eq,
+                  color: _selectedMode == AssistantMode.manual
+                      ? Colors.white
+                      : Colors.green,
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'Manual',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: _selectedMode == AssistantMode.manual
+                        ? Colors.white
+                        : Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  void _onModeChanged(AssistantMode newMode) {
+    setState(() {
+      _selectedMode = newMode;
+    });
+
+    // Call the callback function to notify the mode change
+    widget.onModeChanged(newMode);
   }
 }
--- lib/widgets/nlu_engine_choice.dart
+++ lib/widgets/nlu_engine_choice.dart
@@ -1,3 +1,72 @@
+// import 'package:flutter/material.dart';
+
+// enum NLUEngine { snips, rasa }
+
+// class NLUEngineChoice extends StatefulWidget {
+//   final Function(NLUEngine) onEngineChanged;
+
+//   const NLUEngineChoice({Key? key, required this.onEngineChanged})
+//       : super(key: key);
+
+//   @override
+//   State<NLUEngineChoice> createState() => _NLUEngineChoiceState();
+// }
+
+// class _NLUEngineChoiceState extends State<NLUEngineChoice> {
+//   NLUEngine nluView = NLUEngine.snips;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SegmentedButton<NLUEngine>(
+//       segments: const <ButtonSegment<NLUEngine>>[
+//         ButtonSegment<NLUEngine>(
+//             value: NLUEngine.snips,
+//             label: Text('Snips'),
+//             icon: Icon(Icons.settings_suggest)),
+//         ButtonSegment<NLUEngine>(
+//             value: NLUEngine.rasa,
+//             label: Text('RASA'),
+//             icon: Icon(Icons.settings_suggest)),
+//       ],
+//       selected: <NLUEngine>{nluView},
+//       onSelectionChanged: (Set<NLUEngine> newSelection) {
+//         final newEngine = newSelection.first;
+//         setState(() {
+//           // By default there is only a single segment that can be
+//           // selected at one time, so its value is always the first
+//           // item in the selected set.
+//           nluView = newEngine;
+//         });
+//         // Call the callback function to notify the mode change
+//         widget.onEngineChanged(newEngine);
+//       },
+//       style: ButtonStyle(
+//           side: MaterialStateProperty.all<BorderSide>(
+//             BorderSide(
+//               width: 0, // Remove border width
+//               color: Colors.transparent, // Make border transparent
+//             ),
+//           ),
+//           backgroundColor: MaterialStateProperty.resolveWith<Color>(
+//             (states) {
+//               if (states.contains(MaterialState.selected)) {
+//                 return Colors.green; // Color when pressed
+//               }
+//               // Add more conditions for other states as needed
+//               return Colors.white; // Default color
+//             },
+//           ),
+//           foregroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+//             if (states.contains(MaterialState.selected)) {
+//               return Colors.white; // Color when pressed
+//             }
+//             // Add more conditions for other states as needed
+//             return Colors.green;
+//           })),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 enum NLUEngine { snips, rasa }
@@ -13,56 +82,117 @@ class NLUEngineChoice extends StatefulWidget {
 }
 
 class _NLUEngineChoiceState extends State<NLUEngineChoice> {
-  NLUEngine nluView = NLUEngine.snips;
+  late NLUEngine _selectedEngine;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedEngine = NLUEngine.snips; // Initialize the selection
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SegmentedButton<NLUEngine>(
-      segments: const <ButtonSegment<NLUEngine>>[
-        ButtonSegment<NLUEngine>(
-            value: NLUEngine.snips,
-            label: Text('Snips'),
-            icon: Icon(Icons.settings_suggest)),
-        ButtonSegment<NLUEngine>(
-            value: NLUEngine.rasa,
-            label: Text('RASA'),
-            icon: Icon(Icons.settings_suggest)),
-      ],
-      selected: <NLUEngine>{nluView},
-      onSelectionChanged: (Set<NLUEngine> newSelection) {
-        final newEngine = newSelection.first;
-        setState(() {
-          // By default there is only a single segment that can be
-          // selected at one time, so its value is always the first
-          // item in the selected set.
-          nluView = newEngine;
-        });
-        // Call the callback function to notify the mode change
-        widget.onEngineChanged(newEngine);
-      },
-      style: ButtonStyle(
-          side: MaterialStateProperty.all<BorderSide>(
-            BorderSide(
-              width: 0, // Remove border width
-              color: Colors.transparent, // Make border transparent
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        InkWell(
+          onTap: () => _onEngineChanged(NLUEngine.snips),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            bottomLeft: Radius.circular(20.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 17.5, vertical: 5.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                bottomLeft: Radius.circular(20.0),
+              ),
+              color: _selectedEngine == NLUEngine.snips
+                  ? Colors.green
+                  : Colors.white,
+              border: Border.all(
+                color: Colors.transparent,
+              ),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  _selectedEngine == NLUEngine.snips
+                      ? Icons.check
+                      : Icons.settings_suggest,
+                  color: _selectedEngine == NLUEngine.snips
+                      ? Colors.white
+                      : Colors.green,
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'Snips',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: _selectedEngine == NLUEngine.snips
+                        ? Colors.white
+                        : Colors.green,
+                  ),
+                ),
+              ],
             ),
           ),
-          backgroundColor: MaterialStateProperty.resolveWith<Color>(
-            (states) {
-              if (states.contains(MaterialState.selected)) {
-                return Colors.green; // Color when pressed
-              }
-              // Add more conditions for other states as needed
-              return Colors.white; // Default color
-            },
+        ),
+        InkWell(
+          onTap: () => _onEngineChanged(NLUEngine.rasa),
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            bottomRight: Radius.circular(20.0),
           ),
-          foregroundColor: MaterialStateProperty.resolveWith<Color>((states) {
-            if (states.contains(MaterialState.selected)) {
-              return Colors.white; // Color when pressed
-            }
-            // Add more conditions for other states as needed
-            return Colors.green;
-          })),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 17.5, vertical: 5.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20.0),
+                bottomRight: Radius.circular(20.0),
+              ),
+              color: _selectedEngine == NLUEngine.rasa
+                  ? Colors.green
+                  : Colors.white,
+              border: Border.all(
+                color: Colors.transparent,
+              ),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  _selectedEngine == NLUEngine.rasa
+                      ? Icons.check
+                      : Icons.settings_suggest,
+                  color: _selectedEngine == NLUEngine.rasa
+                      ? Colors.white
+                      : Colors.green,
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'RASA',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: _selectedEngine == NLUEngine.rasa
+                        ? Colors.white
+                        : Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  void _onEngineChanged(NLUEngine newEngine) {
+    setState(() {
+      _selectedEngine = newEngine;
+    });
+
+    // Call the callback function to notify the engine change
+    widget.onEngineChanged(newEngine);
   }
 }
